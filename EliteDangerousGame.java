d3c86c9e2f76b95317123c754513be3b56b1a042
evargaspatrick <151550703+evargaspatrick@users.noreply.github.com>
2024-01-10 17:11:54 -0500

    Small Changes
    Nothing noteworthy.
    Trying to figure out how to make everything callable from a method.

---
+++ EliteDangerousGame.java
@@ -0,0 +1,23 @@
+package Engine1;
+import java.util.Scanner;
+
+public class EliteDangerousGame {
+
+	public static void main (String[] args) {
+		//This is supposed to be the game class, it's going to call all other classes and their methods here
+				// Going to have to rename the other class titled "EliteDangerous"
+				// Thinking about adding some interface for this screen and of course images for other classes and methods
+				// like the pictures for each ship appearing and pictures of their weapons.
+		
+		EliteDangerousIntro intro = new EliteDangerousIntro();
+		EliteGui start = new EliteGui();
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
