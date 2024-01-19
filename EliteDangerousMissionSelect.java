5836c0f70564b5a858af151e6936671921a80b9f
evargaspatrick <151550703+evargaspatrick@users.noreply.github.com>
2024-01-19 09:24:59 -0500

    Delete EliteDangerousMissionSelect.java

--- EliteDangerousMissionSelect.java
+++
@@ -1,9 +0,0 @@
-package Engine1;
-
-public class EliteDangerousMissionSelect {
-
-public static void main (String args[]) {
-	// I have no idea what this class was to be used for.
-	// Maybe as a reception desk for missions. Yeah thats it.
-}
-}
